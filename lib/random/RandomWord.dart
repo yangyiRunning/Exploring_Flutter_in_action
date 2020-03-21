f9543e7b683977c6dd030dc3229ff9468eca3149
yangyi <yangyirunning@163.com>
2020-03-22 00:48:04 +0800

    显示一张图片

--- lib/random/RandomWord.dart
+++ lib/random/RandomWord.dart
@@ -12,9 +12,15 @@ class RandomWord extends StatelessWidget {
         title: Text("随机显示一串英文字符串"),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Text(wordPair.toString()),
-      ),
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              Text(wordPair.toString()),
+              Column(
+                children: <Widget>[Image.asset("assets/ic_mario.png")],
+              )
+            ],
+          )),
     );
   }
 }
