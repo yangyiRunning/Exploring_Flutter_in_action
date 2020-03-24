fd652a456eaf5b4b3f4dab41a036af105c499f35
yangyi <yangyirunning@163.com>
2020-03-24 22:30:33 +0800

    1. 删除超出github上传文件大小限制的ttf字体文件  2. 修改词的落款

--- lib/widget/TextStyleWidget.dart
+++ lib/widget/TextStyleWidget.dart
@@ -62,8 +62,8 @@ class TextStyleWidget extends StatelessWidget {
                 "怒发冲冠，凭阑处、潇潇雨歇。抬望眼，仰天长啸，壮怀激烈。三十功名尘与土，八千里路云和月。莫等闲、白了少年头，空悲切。靖康耻，犹未雪；臣子恨，何时灭！驾长车、踏破贺阑山缺。壮志饥餐胡虏肉，笑谈渴饮匈奴血。待从头、收拾旧山河，朝天阙。",
                 style: textStyle),
             Text(
-              "皇国興廃、ここに戦す",
-              style: TextStyle(fontFamily: "HYQinChuanManShuW", fontSize: 20),
+              "——宋·岳飞",
+              style: TextStyle(fontFamily: "HanyiSentySuciTablet", fontSize: 30)//也可以不用声明直接在此处new
             )
           ],
         ),
