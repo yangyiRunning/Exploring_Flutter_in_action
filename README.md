c6f8d06593b88b397894af23df4cdb6f9d7c91c2
yangyi <yangyirunning@163.com>
2020-04-08 23:37:41 +0800

    readme: 正片(Action)

--- README.md
+++ README.md
@@ -63,6 +63,17 @@
 
 ---
 
+## 正片(Action)
+
+|  |  |  | |
+| :---:  | :---: | :---: | :---: |
+| ![](https://tva1.sinaimg.cn/large/00831rSTly1gdmryrmlz5j30u01t0abl.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdmsdeily1j30u01t078v.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms27cfs8j30u01t0mzn.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms2mvb8rj30u01t0tab.jpg) |
+| ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms2zmrnjj30u01t0my3.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms395nsuj30u01t0ad9.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms3s5dnaj30u01t0gq7.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms43few5j30u01t0n1o.jpg) |
+| ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms7mfimij30u01t0q4b.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms7x7kwkj30u01t0mxy.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdmsng1wahj30u01t0q5q.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms8mcoomj30u01t00ut.jpg) |
+| ![](https://tva1.sinaimg.cn/large/00831rSTly1gdms8y1uxpj30u01t0whs.jpg) | ![](https://tva1.sinaimg.cn/large/00831rSTly1gdmsl8d4iej30u01t0grq.jpg) |  |  |
+
+---
+
 ## 整套效果的App下载链接
 
 - Android APK包下载(内测密码:123456) : ![Android APK包下载](https://www.pgyer.com/app/qrcode/yangyiRunning)
@@ -127,4 +138,4 @@
 
 ---
 
-巨人的肩膀: [Flutter官方示例](https://github.com/flutter/flutter)、[Flutter中文网](https://flutterchina.club)
+###  巨人的肩膀: [Flutter官方示例](https://github.com/flutter/flutter)、[Flutter中文网](https://flutterchina.club)
--- lib/container/TransformWidget.dart
+++ lib/container/TransformWidget.dart
@@ -11,218 +11,220 @@ class TransformWidget extends StatelessWidget {
     final arg = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: getAppBar(arg),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    color: Colors.blue,
-                    child: Text(
-                      "原始文本效果",
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Transform(
-                    //矩阵变化时发生在绘制时，而无需重新布局和构建
-                    transform: Matrix4.skewY(0.3),
-                    alignment: FractionalOffset.center,
-                    child: Container(
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+                child: Column(
+                  children: <Widget>[
+                    Container(
                       padding: EdgeInsets.all(10),
                       color: Colors.blue,
                       child: Text(
-                        "左上角原点系，Y轴错切0.3",
+                        "原始文本效果",
                         style: TextStyle(fontSize: 16, color: Colors.white),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Transform(
-                    //矩阵变化时发生在绘制时，而无需重新布局和构建
-                    transform: Matrix4.skewY(0.3),
-                    alignment: Alignment.center,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "屏幕中心原点系，Y轴错切0.3",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Transform(
+                      //矩阵变化时发生在绘制时，而无需重新布局和构建
+                      transform: Matrix4.skewY(0.3),
+                      alignment: FractionalOffset.center,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "左上角原点系，Y轴错切0.3",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Transform(
-                    alignment: FractionalOffset.center,
-                    transform: Matrix4.translationValues(40, 60, 0),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "左上角原点系，横移40竖移60",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Transform(
+                      //矩阵变化时发生在绘制时，而无需重新布局和构建
+                      transform: Matrix4.skewY(0.3),
+                      alignment: Alignment.center,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "屏幕中心原点系，Y轴错切0.3",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Transform(
-                    alignment: Alignment.center,
-                    transform: Matrix4.translationValues(40, 60, 0),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "屏幕中心原点系，横移40竖移60",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Transform(
+                      alignment: FractionalOffset.center,
+                      transform: Matrix4.translationValues(40, 60, 0),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "左上角原点系，横移40竖移60",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 60,
-                  ),
-                  Transform(
-                    alignment: FractionalOffset.center,
-                    transform: Matrix4.rotationX(90),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "左上角原点系，x轴方向旋转90度",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.translationValues(40, 60, 0),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "屏幕中心原点系，横移40竖移60",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Transform(
-                    alignment: Alignment.center,
-                    transform: Matrix4.rotationX(90),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "屏幕中心原点系，x轴方向旋转90度",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Transform(
+                      alignment: FractionalOffset.center,
+                      transform: Matrix4.rotationX(90),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "左上角原点系，x轴方向旋转90度",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Transform(
-                    alignment: FractionalOffset.center,
-                    transform: Matrix4.rotationY(90),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "左上角原点系，y轴方向旋转90度",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.rotationX(90),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "屏幕中心原点系，x轴方向旋转90度",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Transform(
-                    alignment: Alignment.center,
-                    transform: Matrix4.rotationY(90),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "屏幕中心原点系，y轴方向旋转90度",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Transform(
+                      alignment: FractionalOffset.center,
+                      transform: Matrix4.rotationY(90),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "左上角原点系，y轴方向旋转90度",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 120,
-                  ),
-                  Transform(
-                    alignment: FractionalOffset.center,
-                    transform: Matrix4.rotationZ(90),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "左上角原点系，Z轴方向旋转90度",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.rotationY(90),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "屏幕中心原点系，y轴方向旋转90度",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 90,
-                  ),
-                  Transform(
-                    alignment: Alignment.center,
-                    transform: Matrix4.rotationZ(90),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "屏幕中心原点系，Z轴方向旋转90度",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 120,
+                    ),
+                    Transform(
+                      alignment: FractionalOffset.center,
+                      transform: Matrix4.rotationZ(90),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "左上角原点系，Z轴方向旋转90度",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 90,
-                  ),
-                  Transform.scale(
-                    alignment: FractionalOffset.center,
-                    scale: 0.5,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "左上角原点系，缩小为原来的一半",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 90,
+                    ),
+                    Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.rotationZ(90),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "屏幕中心原点系，Z轴方向旋转90度",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Transform.scale(
-                    alignment: Alignment.center,
-                    scale: 0.5,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "屏幕中心原点系，缩小为原来的一半",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 90,
+                    ),
+                    Transform.scale(
+                      alignment: FractionalOffset.center,
+                      scale: 0.5,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "左上角原点系，缩小为原来的一半",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  RotatedBox(
-                    quarterTurns: 1,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(
-                        "布局阶段旋转90度，真正改变了UI控件的大小和方向，而不仅仅是在视觉上",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Transform.scale(
+                      alignment: Alignment.center,
+                      scale: 0.5,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "屏幕中心原点系，缩小为原来的一半",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  )
-                ],
-              )),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    RotatedBox(
+                      quarterTurns: 1,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.blue,
+                        child: Text(
+                          "布局阶段旋转90度，真正改变了UI控件的大小和方向，而不仅仅是在视觉上",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                )),
+          ),
         ),
       ),
     );
