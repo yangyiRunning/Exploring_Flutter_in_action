97d0d5c9099ecc436e356ad997ea221563565fdd
yangyi <yangyirunning@163.com>
2020-04-09 23:18:30 +0800

    上传演示效果gif图片

--- lib/gesture/GestureRecognizerTextWidget.dart
+++ lib/gesture/GestureRecognizerTextWidget.dart
@@ -37,70 +37,77 @@ class GestureRecognizerTextState extends State<GestureRecognizerTextWidget> {
     return Scaffold(
       appBar: getAppBar(arg),
       body: Container(
-        alignment: Alignment.center,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        //因为TextSpan不是widget，所以不能直接使用GestureDetector去包装这个child
-        //但是其有recognizer节点，故可以尽情的设置Flutter提供的一系列GestureRecognizer实现
-        child: Text.rich(TextSpan(
-          children: [
-            TextSpan(
-              text: "原始",
-              style: TextStyle(
-                color: Colors.blue,
-              ),
-            ),
-            TextSpan(
-                text: "点击变粗",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w900,
-                ),
-                //..语法为级联操作，允许对同一个对象进行一系列操作
-                recognizer: tapGestureRecognizer2
-                  ..onTap = () {
-                    setState(() {
-                      isBold = !isBold;
-                    });
-                  }),
-            TextSpan(
-                text: "点击变大",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: isBig ? 48 : 16,
-                ),
-                recognizer: tapGestureRecognizer3
-                  ..onTap = () {
-                    setState(() {
-                      isBig = !isBig;
-                    });
-                  }),
-            TextSpan(
-                text: "点击变美",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontFamily: isBeautiful ? "HanyiSentySuciTablet" : "",
-                ),
-                recognizer: tapGestureRecognizer4
-                  ..onTap = () {
-                    setState(() {
-                      isBeautiful = !isBeautiful;
-                    });
-                  }),
-            TextSpan(
-                text: "置灰",
-                style: TextStyle(
-                  color: isGrey ? Colors.grey : Colors.blue,
-                ),
-                recognizer: tapGestureRecognizer5
-                  ..onTap = () {
-                    setState(() {
-                      isGrey = !isGrey;
-                    });
-                  })
-          ],
-        )),
-      ),
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          //因为TextSpan不是widget，所以不能直接使用GestureDetector去包装这个child
+          //但是其有recognizer节点，故可以尽情的设置Flutter提供的一系列GestureRecognizer实现
+          child: Wrap(
+            children: <Widget>[
+              Text.rich(TextSpan(
+                children: [
+                  TextSpan(
+                    text: "原始",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  TextSpan(
+                      text: "点击变粗",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blue,
+                        fontWeight: isBold ? FontWeight.w900 : FontWeight.w100,
+                      ),
+                      //..语法为级联操作，允许对同一个对象进行一系列操作
+                      recognizer: tapGestureRecognizer2
+                        ..onTap = () {
+                          setState(() {
+                            isBold = !isBold;
+                          });
+                        }),
+                  TextSpan(
+                      text: "点击变大",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: isBig ? 48 : 20,
+                      ),
+                      recognizer: tapGestureRecognizer3
+                        ..onTap = () {
+                          setState(() {
+                            isBig = !isBig;
+                          });
+                        }),
+                  TextSpan(
+                      text: "点击变美",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blue,
+                        fontFamily: isBeautiful ? "HanyiSentySuciTablet" : "",
+                      ),
+                      recognizer: tapGestureRecognizer4
+                        ..onTap = () {
+                          setState(() {
+                            isBeautiful = !isBeautiful;
+                          });
+                        }),
+                  TextSpan(
+                      text: "置灰",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: isGrey ? Colors.blue : Colors.grey,
+                      ),
+                      recognizer: tapGestureRecognizer5
+                        ..onTap = () {
+                          setState(() {
+                            isGrey = !isGrey;
+                          });
+                        })
+                ],
+              )),
+            ],
+          )),
     );
   }
 }
