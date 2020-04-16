bd0594607c722c39ca20e8c0d368f08b89343d9b
yangyi <yangyirunning@163.com>
2020-04-17 00:05:40 +0800

    拆分App入口和main界面

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import 'common/CommonNotification.dart';
+import 'common/CommonShowModel.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  AppThemeNotification appThemeNotification = new AppThemeNotification(true);
+  bool isFABShow = true;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      floatingActionButton: isFABShow
+          ? FloatingActionButton(
+              child: Icon(Icons.format_paint),
+              onPressed: () {
+                appThemeNotification.isLight = !appThemeNotification.isLight;
+                appThemeNotification.dispatch(context);
+              },
+            )
+          : null,
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: NotificationListener(
+        onNotification: (notification) {
+          if (notification.runtimeType == ScrollUpdateNotification) {
+            setState(() {
+              isFABShow = false;
+            });
+          }
+          if (notification.runtimeType == ScrollEndNotification ||
+              notification.runtimeType == ScrollStartNotification) {
+            setState(() {
+              isFABShow = true;
+            });
+          }
+          return true;
+        },
+        child: Scrollbar(
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+              childAspectRatio: 1.5,
+            ),
+            shrinkWrap: true,
+            padding: EdgeInsets.all(10),
+            itemCount: getMainList(context).length,
+            itemBuilder: (BuildContext context, int index) {
+              if (index % 2 == 0) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    color: Colors.amberAccent[100],
+                    child: getMainList(context)[index],
+                  ),
+                );
+              } else {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    color: Colors.deepPurple[100],
+                    child: getMainList(context)[index],
+                  ),
+                );
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'MyHomePage.dart';
 import 'common/CommonNotification.dart';
 import 'event/EventBusSecondWidget.dart';
 import 'event/EventBusWidget.dart';
@@ -23,7 +25,6 @@ import 'theme/ThemeSwitchManagerWidget.dart';
 import 'touch/TouchManagerWidget.dart';
 import 'touch/TouchPadWidget.dart';
 import 'touch/TouchBubbleWidget.dart';
-import 'common/CommonShowModel.dart';
 import 'container/BoxContainerWidget.dart';
 import 'container/ClipWidget.dart';
 import 'container/ContainerManagerWidget.dart';
@@ -73,6 +74,8 @@ import 'layout/LinearLayoutWidget.dart';
 void main() {
   //是否开启布局边界
 //  debugPaintSizeEnabled = true;
+  debugDefaultTargetPlatformOverride = TargetPlatform.android;
+  print(defaultTargetPlatform);
   runApp(MyApp());
 }
 
@@ -205,103 +208,6 @@ class MyAppState extends State<MyApp> {
 //      },
         //应用首页面
         home: MyHomePage(title: "$appName"),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  MyHomePageState createState() => MyHomePageState();
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  AppThemeNotification appThemeNotification = new AppThemeNotification(true);
-  bool isFABShow = true;
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      floatingActionButton: isFABShow
-          ? FloatingActionButton(
-              child: Icon(Icons.format_paint),
-              onPressed: () {
-                appThemeNotification.isLight = !appThemeNotification.isLight;
-                appThemeNotification.dispatch(context);
-              },
-            )
-          : null,
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: NotificationListener(
-        onNotification: (notification) {
-          if (notification.runtimeType == ScrollUpdateNotification ||
-              notification.runtimeType == ScrollStartNotification) {
-            setState(() {
-              isFABShow = false;
-            });
-          }
-          if (notification.runtimeType == ScrollEndNotification) {
-            setState(() {
-              isFABShow = true;
-            });
-          }
-          return true;
-        },
-        child: Scrollbar(
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 5,
-              crossAxisSpacing: 5,
-              childAspectRatio: 1.5,
-            ),
-            shrinkWrap: true,
-            padding: EdgeInsets.all(10),
-            itemCount: getMainList(context).length,
-            itemBuilder: (BuildContext context, int index) {
-              if (index % 2 == 0) {
-                return ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    color: Colors.amberAccent[100],
-                    child: getMainList(context)[index],
-                  ),
-                );
-              } else {
-                return ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    color: Colors.deepPurple[100],
-                    child: getMainList(context)[index],
-                  ),
-                );
-              }
-            },
-          ),
-        ),
       ),
     );
   }
